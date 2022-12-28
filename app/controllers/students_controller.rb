c2282bc5b5ad0ab972ae14724228a7929a7889c1
Tasreen Mowreen <tmowreen97@gmail.com>
2022-12-27 23:46:22 -0500

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,22 @@
 class StudentsController < ApplicationController
-
+  
   def index
     students = Student.all
-    render json: students
+    if (params.include?(:first_name))==true
+      student = Student.where('first_name=?',params[:first_name])
+      render json: student
+    elsif (params.include?(:last_name))==true
+      student = Student.where('last_name=?',params[:last_name])
+      render json: student
+    else 
+        render json: students
+    end
+  
+  end
+
+  def find_student
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get '/students/:id', to: 'students#find_student'
+  get '/students/:name', to: 'students#index'
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+
+
+
 end
