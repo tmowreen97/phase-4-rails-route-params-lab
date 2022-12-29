b336976c774ab10e685b5b533c258096a24988a4
Tasreen Mowreen <tmowreen97@gmail.com>
2022-12-28 23:55:38 -0500

    passing!

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,15 +2,26 @@ class StudentsController < ApplicationController
   
   def index
     students = Student.all
-    if (params.include?(:first_name))==true
-      student = Student.where('first_name=?',params[:first_name])
-      render json: student
-    elsif (params.include?(:last_name))==true
-      student = Student.where('last_name=?',params[:last_name])
-      render json: student
-    else 
-        render json: students
+    students_with_name=[]
+    if params[:name]
+      name = params[:name].capitalize()
+      students_with_name.push(Student.where('first_name=?', name))
+      students_with_name.push(Student.where('last_name=?', name))
+      render json: students_with_name.flatten!
+    else
+      render json: students
     end
+
+
+    # if (params.include?(:first_name))==true
+    #   student = Student.where('first_name=?',params[:first_name])
+    #   render json: student
+    # elsif (params.include?(:last_name))==true
+    #   student = Student.where('last_name=?',params[:last_name])
+    #   render json: student
+    # else 
+    #     render json: students
+    # end
   
   end
 
