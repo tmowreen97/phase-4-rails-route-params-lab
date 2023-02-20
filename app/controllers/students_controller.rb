7f22b60869cbb067a01c1106ab7d861516d44a3b
Tasreen Mowreen <tmowreen97@gmail.com>
2023-02-20 13:52:38 -0500

    updated

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,33 +1,18 @@
 class StudentsController < ApplicationController
   
-  def index
-    students = Student.all
-    students_with_name=[]
-    if params[:name]
-      name = params[:name].capitalize()
-      students_with_name.push(Student.where('first_name=?', name))
-      students_with_name.push(Student.where('last_name=?', name))
-      render json: students_with_name.flatten!
-    else
-      render json: students
-    end
-
-
-    # if (params.include?(:first_name))==true
-    #   student = Student.where('first_name=?',params[:first_name])
-    #   render json: student
-    # elsif (params.include?(:last_name))==true
-    #   student = Student.where('last_name=?',params[:last_name])
-    #   render json: student
-    # else 
-    #     render json: students
-    # end
-  
+  def show 
+    student = Student.find(params[:id])
+    render json: student, status: :ok
   end
 
-  def find_student
-    student = Student.find(params[:id])
-    render json: student
+  def index
+    if params[:name]
+      student = Student.where("first_name = ? or last_name = ?", params[:name].capitalize(), params[:name].capitalize())
+      render json: student, status: :ok
+    else
+      students = Student.all
+      render json: students, status: :ok
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get '/students/:id', to: 'students#find_student'
-  get '/students/:name', to: 'students#index'
+
+  get '/students/:id', to: 'students#show'
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
